60d0570fe81bef4907e0d472c601c40356169f14
Андрій Коротков <andriikorotkov.vvv@gmail.com>
2026-05-07 19:29:43 +0200

    improve patform(added features ) and changed main

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from content.book import Book
 from content.article import Article
 from content.poem import Poem
-
 from users.regular import Regular
 from users.premium import PremiumUser
-
 from recommendation.genre import Genre
 from recommendation.trending import Trend
-
 from platform import ReadingPlatform
-
-
 def main():
     contents = [Book(),Article(),Poem()]
     user = PremiumUser()
@@ -19,7 +14,15 @@ def main():
     print("----Reading----")
     for content in contents:
         print(platform.read_content(content))
-        print(platform.get_history())
+    print("---- History ----")
+    print(platform.get_history())
+    print("---- Statistics ----")
+    print(platform.get_statistics())
+    print("---- Recommendations ----")
+    print(platform.get_recommendations())
+    platform.recommendation = Trend()
+    print("---- Trending Recommendations ----")
+    print(platform.get_recommendations())
 if __name__ == "__main__":
     main()
 
--- platform.py
+++ platform.py
@@ -1,17 +1,23 @@
-
+from users.base import User
+from recommendation.base import Recommendation
 class ReadingPlatform:
-    def __init__(self, user, recommendation):
+    def __init__(self, user: User, recommendation: Recommendation):
         self.user = user
         self.recommendation = recommendation
         self.history = []
-
     def read_content(self, content):
         result = self.user.read(content)
-        self.history.append(content.__class__.__name__)
+        self.history.append(content)
         return result
-
     def get_recommendations(self):
         return self.recommendation.suggest()
-
     def get_history(self):
-        return self.history
+        return [item.__class__.__name__ for item in self.history]
+    def clear_history(self):
+        self.history.clear()
+    def change_user(self, user: User):
+        self.user = user
+    def get_statistics(self):
+        return {
+            "total_read": len(self.history)
+        }
